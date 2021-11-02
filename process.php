476edb22e6a64dd54efeed1ab261dc8db9621443
elchukko <41019446+elchukko@users.noreply.github.com>
2021-11-01 23:06:56 -0700

    Add files via upload

--- process.php
+++ process.php
@@ -15,7 +15,42 @@ if(isset($_POST)){
 		$stmtinsert = $db->prepare($sql);
 		$result = $stmtinsert->execute([$firstname, $lastname, $email, $artist, $rate]);
 		if($result){
-			echo 'Successfully saved.';
+			echo 'You made an appt with';
+                        echo " ";
+                        echo $artist;
+                        echo ",";
+                        echo " ";
+                        echo "they will email to follow up with you.";
+		}else{
+			echo 'There were erros while saving the data.';
+		}
+}else{
+	echo 'No data';
+}
+?>
+<?php
+require_once('register/config.php');
+?>
+<?php
+
+if(isset($_POST)){
+
+	$firstname 		= $_POST['firstname'];
+	$lastname 		= $_POST['lastname'];
+	$drink 			= $_POST['drink'];
+	$size         	= $_POST['size'];
+	$tip 		= sha1($_POST['tip']);
+
+		$sql = "INSERT INTO coffeereceipt (firstname, lastname, drink, size, tip ) VALUES(?,?,?,?,?)";
+		$stmtinsert = $db->prepare($sql);
+		$result = $stmtinsert->execute([$firstname, $lastname, $drink, $size, $tip]);
+		if($result){
+			echo 'You made an appt with';
+                        echo " ";
+                        echo $artist;
+                        echo ",";
+                        echo " ";
+                        echo "they will email to follow up with you.";
 		}else{
 			echo 'There were erros while saving the data.';
 		}
